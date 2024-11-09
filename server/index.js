4e931fc1bf258bab07b59628e330674d25cafabd
Kobaiko <Kobaiko@users.noreply.github.com>
2024-11-09 23:02:29 +0200

    Hope is not here

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(express.json());
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.VITE_OPENAI_API_KEY
 });
 
 app.post('/api/generate-subtasks', async (req, res) => {
